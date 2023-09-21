082610d64ee5048254d0e05a7a19c3242b39737f
Bak <95734631+BAKHONGMIN@users.noreply.github.com>
2023-09-21 09:14:31 +0900

    useContext 알아보기
    useContext 알아보기

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import configureStore from "./store/configureStrore";
+// import { Provider } from "react-redux";
+// import configureStore from "./store/configureStrore";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
@@ -13,9 +13,9 @@ root.render(
   // {/* Provider는 리액트 컴포넌트로 모든 컴포넌트의 부모 컴포넌트처럼 동작하며 저장소 데이터를 제공한다
   //   더불어, Provider는 configureStore함수의 반환값을 받아서 초기화된 저장소를 전달받는다.
   // */}
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>
+  // <Provider store={configureStore()}>
+  <App />
+  // </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
